c4b4fb4e638261bdad3a8c46848d47b839a8389a
LuisEnrDiaz <luis.enr.diaz.garcia@gmail.com>
2022-11-28 19:31:02 +0100

    Config initial Sonar and add db connect and test

---
+++ src/db.connect.test.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+import { dbConnect } from './db.connect.js';
+
+const spiConnect = jest.spyOn(mongoose, 'connect');
+
+describe('Given "dbConnect"', () => {
+    describe('When the environment is "test"', () => {
+        test('Then it should connect with DB', async () => {
+            const result = await dbConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe(
+                'FinalProjectTesting'
+            );
+        });
+    });
+    describe('When the environment is not "test"', () => {
+        test('Then it should connect with DB', async () => {
+            process.env.NODE_ENV = 'development';
+            const result = await dbConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe('FinalProject');
+        });
+    });
+    afterEach(() => {
+        mongoose.disconnect();
+    });
+});
