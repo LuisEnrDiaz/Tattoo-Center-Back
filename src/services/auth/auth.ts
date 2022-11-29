f9d9f296ba3c59802e181a989408b80b659f63ab
LuisEnrDiaz <luis.enr.diaz.garcia@gmail.com>
2022-11-29 10:31:43 +0100

    Add auth

---
+++ src/services/auth/auth.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import bc from 'bcryptjs';
+
+import { SECRET } from '../../config.js';
+
+export type TokenPayload = {
+    name: string;
+    password: string;
+};
+
+export const getSecret = (secret = SECRET) => {
+    if (typeof secret !== 'string' || secret === '') {
+        throw new Error('Bad Secret for token cretion');
+    }
+    return secret;
+};
+
+export const createToken = (payload: TokenPayload) => {
+    return jwt.sign(payload, getSecret());
+};
+
+export const readToken = (token: string) => {
+    const payload = jwt.verify(token, getSecret());
+    return payload as jwt.JwtPayload;
+};
+
+export const passwordEncrypt = (password: string) => {
+    return bc.hash(password, 10);
+};
