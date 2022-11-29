e16681925e4c6cf0b53f70e460126291f58774fc
LuisEnrDiaz <luis.enr.diaz.garcia@gmail.com>
2022-11-29 18:10:07 +0100

    Correction error test user

--- src/middleware/corsMiddleware/cors.ts
+++ src/middleware/corsMiddleware/cors.ts
@@ -3,6 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 export const setCors = (req: Request, res: Response, next: NextFunction) => {
     const origin = req.header('origin') || '*';
 
-    res.setHeader('Access-Control-Allow-Origin', origin as string);
+    res.setHeader('Access-Control-Allow-Origin', origin);
     next();
 };
--- src/repository/userRepository/user.test.ts
+++ src/repository/userRepository/user.test.ts
@@ -13,8 +13,8 @@ describe('Given UserRepository', () => {
 
     beforeAll(async () => {
         await dbConnect();
-        await repository.getUserModel().deleteMany();
-        await repository.getUserModel().insertMany(mockData);
+        repository.getUserModel().deleteMany();
+        repository.getUserModel().insertMany(mockData);
 
         const data = await repository.getUserModel().find();
         testIds = [data[0].id, data[1].id];
