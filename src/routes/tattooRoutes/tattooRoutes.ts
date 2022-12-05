0a37b87c480c2c59656d4b3602ed10c5cb208993
LuisEnrDiaz <luis.enr.diaz.garcia@gmail.com>
2022-12-05 18:39:44 +0100

    add interceptors

--- src/routes/tattooRoutes/tattooRoutes.ts
+++ src/routes/tattooRoutes/tattooRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { TattooController } from '../../controllers/tattooController/tattooController.js';
+import { logged, who } from '../../middleware/interceptors/interceptors.js';
 import { TattooRepository } from '../../repository/tattooRepository/tattooRepository.js';
 import { UserRepository } from '../../repository/userRepository/userRepository.js';
 
@@ -13,6 +14,16 @@ const controller = new TattooController(
 
 tattooRoutes.get('/', controller.getAllTattoo.bind(controller));
 tattooRoutes.get('/:id', controller.getTattoo.bind(controller));
-tattooRoutes.post('/:id', controller.createTattoo.bind(controller));
-tattooRoutes.patch('/:id', controller.updateTattoo.bind(controller));
-tattooRoutes.delete('/:id', controller.deleteTattoo.bind(controller));
+tattooRoutes.post('/:id', logged, controller.createTattoo.bind(controller));
+tattooRoutes.patch(
+    '/:id',
+    logged,
+    who,
+    controller.updateTattoo.bind(controller)
+);
+tattooRoutes.delete(
+    '/:id',
+    logged,
+    who,
+    controller.deleteTattoo.bind(controller)
+);
